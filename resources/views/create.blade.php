e96e89a16cc7fa23b9f3f9813d572d9bbc8627f7
kenta521 <kenta521trial@gmail.com>
2022-05-23 15:40:16 +0900

    css

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
+        <link rel="stylesheet" href="{{ asset('css/create.css')}}">
     </head>
     <body>
         <h1>恋愛相談所</h1>
